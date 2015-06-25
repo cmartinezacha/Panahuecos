23b857fb89c36e5b53c11862a50434d87b891cde
Alvaro Sosa <alvarososa123@gmail.com>
2015-06-25 10:54:49 -0500

    Cambiar css de navbar. cambiar src's de internet a locales.

--- static/nuestro.js
+++ static/nuestro.js
@@ -1,35 +1,46 @@
 $(function($){
-		    	$.datepicker.regional['es'] = {
-		    		closeText: 'Cerrar',
-		    		prevText: '<Ant',
-		    		nextText: 'Sig>',
-		    		currentText: 'Hoy',
-		    		monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
-		    		monthNamesShort: ['Ene','Feb','Mar','Abr', 'May','Jun','Jul','Ago','Sep', 'Oct','Nov','Dic'],
-		    		dayNames: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
-		    		dayNamesShort: ['Dom','Lun','Mar','Mié','Juv','Vie','Sáb'],
-		    		dayNamesMin: ['Do','Lu','Ma','Mi','Ju','Vi','Sá'],
-		    		weekHeader: 'Sm',
-		    		dateFormat: 'dd-mm-yy',
-		    		firstDay: 1,
-		    		isRTL: false,
-		    		showMonthAfterYear: false,
-		    		yearSuffix: ''
-		    	};
-		    	$.datepicker.setDefaults($.datepicker.regional['es']);
-		    });
-		    $(function () {
-		    	$("#fecha").datepicker({
-    			inline : true,
-    			onSelect : function(date){ 
-    			window.location.href ='/'+date  
-    			}
-    		});
-
-		    });
-
-$(document).ready(function () {
-    $('#noticias').click(function(e) {
-        $('#nav_secondary').toggleClass('no-show');
-    });     
+	$.datepicker.regional['es'] = {
+		closeText: 'Cerrar',
+		prevText: '<Ant',
+		nextText: 'Sig>',
+		currentText: 'Hoy',
+		monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+		monthNamesShort: ['Ene','Feb','Mar','Abr', 'May','Jun','Jul','Ago','Sep', 'Oct','Nov','Dic'],
+		dayNames: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+		dayNamesShort: ['Dom','Lun','Mar','Mié','Juv','Vie','Sáb'],
+		dayNamesMin: ['Do','Lu','Ma','Mi','Ju','Vi','Sá'],
+		weekHeader: 'Sm',
+		dateFormat: 'dd-mm-yy',
+		firstDay: 1,
+		isRTL: false,
+		showMonthAfterYear: false,
+		yearSuffix: ''
+	};
+	$.datepicker.setDefaults($.datepicker.regional['es']);
 });
+
+$(function () {
+	$("#fecha").datepicker({
+		inline : true,
+		onSelect : function(date){ 
+			window.location.href ='/'+date  
+		}
+	});
+
+});
+
+$('.navbar li a').click(function(e) {
+  var $this = $(this);
+  if (!$this.hasClass('active')) {
+    $this.addClass('active');
+  }
+  e.preventDefault();
+});
+
+// $(document).ready(function () {
+// 	$('ul.nav > li').click(function (e) {
+// 		e.preventDefault();
+// 		$('ul.nav > li').removeClass('active');
+// 		$(this).addClass('active');                
+// 	});            
+// });
