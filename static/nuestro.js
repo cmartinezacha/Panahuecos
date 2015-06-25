7eec7ea3c6d57e38a500a976afbf1040e35cd13e
Alvaro Sosa <alvarososa123@gmail.com>
2015-06-25 14:02:03 -0500

    Adelantar el nav bar

--- static/nuestro.js
+++ static/nuestro.js
@@ -28,19 +28,3 @@ $(function () {
 	});
 
 });
-
-$('.navbar li a').click(function(e) {
-  var $this = $(this);
-  if (!$this.hasClass('active')) {
-    $this.addClass('active');
-  }
-  e.preventDefault();
-});
-
-// $(document).ready(function () {
-// 	$('ul.nav > li').click(function (e) {
-// 		e.preventDefault();
-// 		$('ul.nav > li').removeClass('active');
-// 		$(this).addClass('active');                
-// 	});            
-// });
